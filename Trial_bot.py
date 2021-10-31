61fd119bc1d20949dc0a14c9e27a53af91aaff45
JeffreyYang6 <yangjeffrey05@gmail.com>
2021-10-31 19:25:01 -0400

    cards

--- Trial_bot.py
+++ Trial_bot.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from decouple import config
+import requests
 
 client = discord.Client()
 TOKEN = config('TOKEN')
@@ -20,5 +21,11 @@ async def on_message(message):
     
     if message.content.startswith(PREFIX + 'gb'):
         await message.channel.send('Goodbye!')
+    
+    if message.content.startswith(PREFIX + 'cards'):
+        deck = requests.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').json()
+        deck_id = deck['deck_id']
+        hand = requests.get(f'https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=2').json()
+        await message.channel.send(hand)
 
 client.run(TOKEN)
